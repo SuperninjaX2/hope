66d9a0bba8b0bd1499806480f5f0977c9211b854
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2024-03-29 20:14:25 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
+let menuBtn = document.querySelector(".menuBtn")
+let nav = document.querySelector('nav')
+
+openNav=()=>{
+  console.log(nav)
+  nav.classList.toggle("hide");
+  nav.classList.add("slide-bottom")
+}
+menuBtn.addEventListener("click", openNav)
+
+
+
+
 
