8245c76b0c60f6ffd62621c62b32025bb580dff1
SuperninjaX2 <80552583+SuperninjaX2@users.noreply.github.com>
2024-03-29 21:44:02 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ openNav=()=>{
   console.log(nav)
   nav.classList.toggle("hide");
   nav.classList.add("slide-bottom")
+  nav.classList.add("fade-in")
 }
 menuBtn.addEventListener("click", openNav)
 
